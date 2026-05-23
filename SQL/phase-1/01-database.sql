057bd3ed18e33b54f711d6a4a8120ca8e2dbf1e3
Adrixn28 <TU_CORREO_DE_GITHUB>
2026-05-23 10:19:59 -0500

    feat: create database & use database.

---
+++ SQL/phase-1/01-database.sql
@@ -0,0 +1,2 @@
+-- To create the database and work with it.
+CREATE DATABASE db_geotour_cartagena;
---
+++ SQL/phase-1/02-usedatabase.sql
@@ -0,0 +1,2 @@
+-- You ought to execute this statement for to be able create tables with it atributes.
+USE db_geotour_cartagena;
